36a44361f649f7ff73069a9ca0f2645fc20eb17f
Akshay Thakur <Akshay@acenet.io>
2023-12-19 14:09:56 +0530

    complete basic implementaion of redux-toolkit

--- redux/src/index.js
+++ redux/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { Counter } from "./features/counter/counter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    {/* <App /> */}
+    <Counter />
   </Provider>
 );
