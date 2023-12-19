8dc42f3be1ddb94c06c898659d63b45eb1a8c8d8
Akshay Thakur <Akshay@acenet.io>
2023-12-19 13:16:43 +0530

    simple state

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">APP</div>;
+  const [count, setCount] = useState(0);
+
+  // function Increment() {
+  //   setCount(count + 1);
+  // }
+
+  // function Decrement() {
+  //   setCount(count - 1);
+  // }
+
+  const Increment = () => {
+    setCount(count + 1);
+  };
+  const Decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={Increment}> + </button>
+      <br />
+      <button onClick={Decrement}>- </button>
+    </div>
+  );
 }
 
 export default App;
