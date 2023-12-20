181e6ded31c5aa126a44f33630a8e94716888b69
Akshay Thakur <Akshay@acenet.io>
2023-12-20 09:11:52 +0530

    rtk querry

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
+import { useGetPokemonByNameQuery } from "./services/pokemon";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+  // const [count, setCount] = useState(0);
 
   // function Increment() {
   //   setCount(count + 1);
@@ -12,19 +14,32 @@ function App() {
   //   setCount(count - 1);
   // }
 
-  const Increment = () => {
-    setCount(count + 1);
-  };
-  const Decrement = () => {
-    setCount(count - 1);
-  };
+  // const Increment = () => {
+  //   setCount(count + 1);
+  // };
+  // const Decrement = () => {
+  //   setCount(count - 1);
+  // };
 
   return (
+    // <div className="App">
+    //   <h1>{count}</h1>
+    //   <button onClick={Increment}> + </button>
+    //   <br />
+    //   <button onClick={Decrement}>- </button>
+    // </div>
+
     <div className="App">
-      <h1>{count}</h1>
-      <button onClick={Increment}> + </button>
-      <br />
-      <button onClick={Decrement}>- </button>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
     </div>
   );
 }
--- redux/src/app/store.js
+++ redux/src/app/store.js
@@ -1,8 +1,28 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import counterReducer from "../features/counter/counterSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
+
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+// Or from '@reduxjs/toolkit/query/react'
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { pokemonApi } from "../services/pokemon";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    // Add the generated reducer as a specific top-level slice
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
